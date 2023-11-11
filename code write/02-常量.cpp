2f67ed04e543f531cc0474166d1c9c76234dd221
willamsc <135994644+willamsc@users.noreply.github.com>
2023-11-11 19:10:36 +0800

    Add files via upload

---
+++ code write/02-常量.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define week 7
+int main02()
+{
+	cout << "一周有几天" << week << "天" << endl;
+	const int month = 12;
+	cout << "一年有" << month << "个月" << endl;
+	return 0;
+}
